b6e18cd39ce566bf40bb3d0cd408ab89f285326f
Simon George <simon@sfcgeorge.co.uk>
2015-07-14 23:19:02 +0100

    Start updating for Spree 3

--- app/models/spree/product_customization.rb
+++ app/models/spree/product_customization.rb
@@ -2,11 +2,11 @@ module Spree
   class ProductCustomization < ActiveRecord::Base
     belongs_to :product_customization_type
     belongs_to :line_item
-    has_many :customized_product_options, :dependent => :destroy
+    has_many :customized_product_options, dependent: :destroy
 
     # price might depend on something contained in the variant
     # (like product property value), so optionally pass that in
-    def price(variant=nil)
+    def price(variant = nil)
       product_customization_type.calculator.compute(self, variant)
     end
 
