05c52db2f4b35b47158532863cb8a15f3c9adf72
Simon George <simon@sfcgeorge.co.uk>
2015-07-17 21:38:54 +0100

    Remove jquery.formatCurrency. Fixes issue #1

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -1,7 +1,6 @@
 module Spree
-
   Order.class_eval do
-    def product_customizations_match(line_item,new_customizations)
+    def product_customizations_match(line_item, new_customizations)
       existing_customizations = line_item.product_customizations
 
       if new_customizations.kind_of? ActiveSupport::HashWithIndifferentAccess
@@ -11,12 +10,12 @@ module Spree
         return false unless existing_customizations.map(&:product_customization_type_id).sort == new_customizations.keys.map(&:to_i).sort
 
         a = []
-        new_customizations.values.each do |ncv_hash| 
+        new_customizations.values.each do |ncv_hash|
           ncv_hash.keys.each do |key|
-            a<< [key.to_i, ncv_hash[key]]
+            a << [key.to_i, ncv_hash[key]]
           end
         end
-  
+
         new_vals = Set.new a
       else
         new_customizations = new_customizations.product_customizations
@@ -28,8 +27,7 @@ module Spree
 
         new_vals = customization_pairs(new_customizations)
       end
-  
-  
+
       # get a list of [customizable_product_option.id,value] pairs
       existing_vals = customization_pairs(existing_customizations)
       # do a set-compare here
@@ -39,13 +37,14 @@ module Spree
     self.register_line_item_comparison_hook(:product_customizations_match)
 
     private
-      # produces a list of [customizable_product_option.id,value] pairs for subsequent comparison
-      def customization_pairs(product_customizations)
-        pairs= product_customizations.map(&:customized_product_options).flatten.map do |m|
-          [m.customizable_product_option.id, m.value.present? ? m.value : m.customization_image.to_s ]
-        end
 
-        Set.new pairs
+    # produces a list of [customizable_product_option.id,value] pairs for subsequent comparison
+    def customization_pairs(product_customizations)
+      pairs = product_customizations.map(&:customized_product_options).flatten.map do |m|
+        [m.customizable_product_option.id, m.value.present? ? m.value : m.customization_image.to_s]
       end
+
+      Set.new pairs
+    end
   end
 end
