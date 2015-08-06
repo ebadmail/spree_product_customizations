f8041694f38d5fbca53a9ead1ae443c45985ffa6
Simon George <simon@sfcgeorge.co.uk>
2015-08-06 11:04:09 +0100

    Fix display of customized line item in order process

--- app/overrides/add_extra_order_details_line_item_description.rb
+++ app/overrides/add_extra_order_details_line_item_description.rb
@@ -1,10 +1,31 @@
-Deface::Override.new(:virtual_path => "spree/shared/_order_details",
-                     :name => "add_extra_order_details_line_item_description",
-                     :insert_bottom => "[data-hook='order_item_description']",
-                     :partial => "spree/shared/extra_order_details_line_item_description")
+# There are so many places Spree duplicated showing a list of line items!
 
-Deface::Override.new(:virtual_path => "spree/admin/shared/_order_details",
-                     :name => "add_extra_order_details_line_item_description_admin",
-                     :insert_bottom => "[data-hook='order_details_line_item_row'] td:first",
-                     :partial => "spree/shared/extra_order_details_line_item_description")
+Deface::Override.new(virtual_path: "spree/shared/_order_details",
+                     name: "add_extra_order_details_line_item_description",
+                     insert_bottom: "[data-hook='order_item_description']",
+                     partial: "spree/shared/extra_order_details_line_item_description")
 
+Deface::Override.new(virtual_path: "spree/shared/_line_item",
+                     name: "add_extra_order_details_line_item_description_2",
+                     insert_bottom: '[data-hook="line_item_description"]',
+                     partial: "spree/shared/extra_order_details_line_item_description")
+
+Deface::Override.new(virtual_path: "spree/admin/orders/_line_item",
+                     name: "add_extra_order_details_line_item_description_admin_2",
+                     insert_bottom: "data-hook='admin_order_form_line_item_row'",
+                     partial: "spree/shared/extra_order_details_line_item_description")
+
+Deface::Override.new(virtual_path: "spree/admin/orders/_line_items",
+                     name: "add_extra_order_details_line_item_description_admin_3",
+                     insert_bottom: '.line-item-name',
+                     partial: "spree/shared/extra_order_details_line_item_description")
+
+Deface::Override.new(virtual_path: "spree/admin/orders/_shipment_manifest",
+                     name: "add_extra_order_details_line_item_description_admin_4",
+                     insert_bottom: '.item-name',
+                     partial: "spree/shared/extra_order_details_line_item_description")
+
+Deface::Override.new(virtual_path: "spree/checkout/_delivery",
+                     name: "add_extra_order_details_line_item_description_admin_5",
+                     insert_bottom: '.item-name',
+                     partial: "spree/shared/extra_order_details_line_item_description")
